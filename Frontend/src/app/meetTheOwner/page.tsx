702786980ebf3611df83188696ca25c93e74ec70
jordandawson3 <jtdawson333@gmail.com>
2024-11-01 14:18:48 -0700

    meetTheOwner page base layout (no nav bar or footer)

--- Frontend/src/app/meetTheOwner/page.tsx
+++ Frontend/src/app/meetTheOwner/page.tsx
@@ -1,3 +1,124 @@
+import './meetTheOwner.css';
+import Image from "next/image";
+import ownerPic from './default_profile.jpg';
+
 export default function page(){
-    return <p></p>
+    return <html>
+    
+    <body> 
+        <div className="text-[black] text-[48px] text-center font-[inter]">Meet The Owner</div>
+        <div className="wrapper">
+            <div className="max-w-[400px] text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] ">
+            
+              <Image src={ownerPic} 
+              alt="owner picture"
+              width={400}
+              height={400}/> 
+              <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Sigfrido Vasquez</div>
+              <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter]">Email: xxxxx@xxx.com</div>
+              <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter]">Phone: (XXX) XXX-XXXX</div>   
+            
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[400px] max-w-[900px]">bio placeholder</div>
+          </div>
+          <div className="text-[48px] text-center pt-[48px] font-[inter] text-[black]">The Dev Team</div>
+          <div className="wrapper">
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+            
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+              
+              
+            </div>
+          </div>
+          <div className="wrapper">
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+            
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+            
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+          </div>
+          <div className="wrapper">
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+            
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+            
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+            <div className="text-[24px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] font-[inter] min-w-[300px] max-w-[400px]">
+
+            <Image src={ownerPic} 
+              alt="dev picture"
+              width={400}
+              height={400}/>
+
+            <div className="text-[18px] text-[black] bg-[white] m-[.4em] p-[.3em] rounded-[3px] flex-[1] text-center font-[inter]">Dev Placeholder</div>
+
+            </div>
+          </div>
+    </body>
+</html>
 }
