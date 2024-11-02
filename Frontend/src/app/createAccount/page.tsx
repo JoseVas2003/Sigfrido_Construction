a5c3b5c432f4bb62876e6dc4b664e45bd816a42b
Nathan Kovak <nathankovak@gmail.com>
2024-11-01 19:16:24 -0700

    Pushing Create Account Page to Main

--- Frontend/src/app/createAccount/page.tsx
+++ Frontend/src/app/createAccount/page.tsx
@@ -1,3 +1,66 @@
+'use client'
+import '../Assets/css/createAccount.modules.css';
+import Navbar from '../navbar/navBar';
+import {clicksOut} from '../navbar/navBar'
+
 export default function page(){
-    return <p></p>
+    return (
+        <div>
+            <Navbar/>
+            <main>
+                <div className="Container" onClick= {() => {clicksOut()}}>
+                    <form className="createAccountForm">
+
+                        <label className="firstNameLabel">First Name </label> <label className="firstNameAsterisk"> *</label> <label className="lastNameLabel">Last Name </label> <label className="lastNameAsterisk"> *</label>
+
+                        <div className="nameContainer">
+                            <input className="firstNameInput" type="text"/>
+                            <input className="lastNameInput" type="text"/>
+                        </div>
+                        
+                        <br></br>
+
+                        <label className="phoneNumberLabel">Phone Number </label> <label className="phoneNumberAsterisk"> *</label>
+
+                        <div className="phoneNumberInputContainer">
+                            <input className="phoneNumberInput" type="tel"/>
+                        </div>
+
+                        <br></br>
+        
+                        <label className="createAccountEmailLabel">Email </label> <label className="createAccountEmailAsterisk"> *</label>
+
+                        <div className="emailInputContainer">
+                            <input className="createAcountEmailInput" type="email"/>
+                        </div>
+
+                        <br></br>
+
+                        <label className="createAccountPasswordLabel">Password </label> <label className="createAccountPasswordAsterisk"> *</label>
+
+                        <div className="passwordContainer">
+                            <input className="createAccountPasswordInput" type="password"/>
+                        </div>
+
+                        <br></br>
+
+                        <label className="confirmPasswordLabel">Confirm Password </label> <label className="confirmPasswordAsterisk"> *</label>
+
+                        <div className="confirmPasswordContainer">
+                            <input className="confirmPasswordInput" type="password"/>
+                        </div>
+            
+                        <br></br>
+
+                        <div className="CreateAccountButtonContainer"> 
+                            <button className="CreateAccountButton">Create Account</button>
+                        </div>
+
+                        <br></br>
+                    </form>
+                </div>
+            </main>
+        </div> 
+                
+    );
 }
